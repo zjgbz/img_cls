6e1d19935ddcfbe4686b91ad30b6e319f807e800
zjgbz <minky.k.jiang@gmail.com>
2021-08-02 15:43:19 +0800

    modified:   src/cellmorpho_train.py

--- src/cellmorpho_train.py
+++ src/cellmorpho_train.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     # step0: setting path
     train_dict_dir_filename = os.path.join(args.img_dict_dir, f"{cfg.label_col}_raw_img_unique_train.csv")
     val_dict_dir_filename = os.path.join(args.img_dict_dir, f"{cfg.label_col}_raw_img_unique_val.csv")
+    print(train_dict_dir_filename, val_dict_dir_filename)
     check_data_dir(train_dict_dir_filename)
     check_data_dir(val_dict_dir_filename)
     train_dict_df = pd.read_csv(train_dict_dir_filename, sep = ",", header = 0, index_col = None)
